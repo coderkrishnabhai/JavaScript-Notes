7fd5caadfe6dc66032a76aeeb82d28da436a2862
KRISHNA KUMAR MANDAL <118360906+coderkrishnabhai@users.noreply.github.com>
2024-07-30 23:53:17 +0530

    Add files via upload

---
+++ javaScriptNotes.js
@@ -0,0 +1,90 @@
+// var let const
+/*
+var num = 23;
+
+{
+    var num = 20;
+}
+
+console.log(num)
+*/
+
+// let
+
+/*
+
+let num = 50;
+
+{
+    let num = 40;
+    console.log(num)
+}
+
+console.log(num)
+
+*/
+
+// const
+
+/*
+const num = 20;
+num = 40;
+
+console.log(num)
+
+// got an error
+
+*/
+
+// data type in javascript
+
+// String
+
+// Numbers
+
+// BigInt
+
+// Boolean
+
+// undefined
+
+// null
+
+// Symbol
+
+// Object
+
+// creating an object
+
+/*
+const myObj = {
+    name  :'krishna',
+    age : 20,
+    height  : 5.6
+}
+
+console.log(myObj.name.toUpperCase())
+
+*/
+
+//functions in javascript
+
+/*
+const myFu = (a,b) =>{
+    return (
+        a + b
+       
+    )
+}
+
+console.log(myFu(12,34))
+
+*/
+
+const myObj = {};
+
+myObj.name = "krishan";
+myObj.age = 20;
+myObj.height = 5.6;
+
+console.log(myObj.name);
